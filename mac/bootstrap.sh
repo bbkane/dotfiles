3141a2134cf48a77b05aa1b294f984cfb98c3927
Benjamin Kane <6081085+bbkane@users.noreply.github.com>
2014-12-26 18:22:19 -0500

    FTB Launcher and updated bootstrap

--- mac/bootstrap.sh
+++ mac/bootstrap.sh
@@ -1,36 +1,54 @@
+: '
+# install command line tools and xcode
 #xcode-select --install
-#sudo gcc --version
+#sudo xcodebuild -license
+'
 
 : '
+# install Homebrew
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew doctor
 '
 #echo export PATH='/usr/local/bin:$PATH' >> ~/.bash_profile
 
 : '
-echo "hi"
+# install Homebrew cask for graphical apps
 brew install caskroom/cask/brew-cask
 brew cask doctor
 '
 
 : '
-echo "hi"
+# Install Git
 brew install git
 git config --global user.email "bbk1524@gmail.com"
 git config --global user.name "Ben-MacbookPro"
 git config --global push.default matching
 '
 
-#brew install vim macvim
-#brew linkapps macvim
-#brew install ack
-#curl -Lo- https://bit.ly/janus-bootstrap | bash
-
-#brew install zsh
-#echo "change shells through preferences in GDoc, then restart"
-#curl -L http://install.ohmyz.sh | sh
+: '
+# get vim , macvim, and janus
+brew install vim macvim
+brew linkapps macvim
+brew install ack
+curl -Lo- https://bit.ly/janus-bootstrap | bash
+'
 
 : '
+# install oh-my-zsh. This might need work
+brew install zsh
+echo "change shells through preferences in GDoc, then restart"
+curl -L http://install.ohmyz.sh | sh
+'
+
+: '
+# Get Anaconda for python
+cd ~
+curl -O http://repo.continuum.io/anaconda3/Anaconda3-2.1.0-MacOSX-x86_64.sh
+bash ~/Anaconda3-2.1.0-MacOSX-x86_64.sh
+'
+
+: '
+# Get my dotfiles
 brew install stow
 rm .zshrc
 cd ~
@@ -39,25 +57,46 @@ cd backup
 stow common
 stow mac
 '
-#brew install clisp pandoc wget fdupes httrack
 
 : '
-cd ~
-#curl -O http://repo.continuum.io/anaconda3/Anaconda3-2.1.0-MacOSX-x86_64.sh
-#bash ~/Anaconda3-2.1.0-MacOSX-x86_64.sh
+# get Lisp
+brew install clisp
+# pandoc is for latex, markdown, etc
+brew install pandoc
+# wget is for downloading power!!!
+brew install wget
+# fdupes erases duplicates in a folder
+brew install fdupes
+# httrack lets you mirror a website easily. Use in a empty folder
+brew install httrack
 '
-#brew cask install firefox skim dropbox google-drive functionflip skype transmission vlc eclipse-java
-#brew cask install alfred
-# echo add Caskroom to Alfed manually
-#echo install the latest JDK from the website
-#brew cask cleanup
-# activate function flip
+
+: '
+# obvious installs
+brew cask install firefox dropbox google-drive skype vlc
+# skim is a really good pdf reader with note exporting abilies I like
+brew cask install skim
+# functionflip is useful for F5 (compile and run in spyder and eclipse)
+brew cask install functionflip
+echo activate function flip for F5
+# transmission is a torrent client. Yay
+brew cask install transmission
+# eclipse is huge. Worth it though
+brew cask install eclipse-java
+echo install the latest JDK from the website
+# Alfred replaces Spotlight Search. Can run applescripts on search
+brew cask install alfred
+echo add Caskroom to Alfed manually
+# clean up leftovers
+brew cask cleanup
+# openemu is a one shop stop emulator frontend for SNES, GBA, etc
 #brew cask install openemu
+'
 
-
-#: '
+: '
 # start getting wallpapers!!
 cd ~
-#git clone https://github.com/EndlesslyCurious/RedditImageGrab.git
+git clone https://github.com/EndlesslyCurious/RedditImageGrab.git
 python2.7 ~/RedditImageGrab/redditdownload.py earthporn ~/Pictures/Wallpapers -score 1000 -num 25
-#'
+echo Change Wallpaper settings in Preferences
+'
