b318f12392e9383ee61757c82281e248fd6488e2
Benjamin Kane <6081085+bbkane@users.noreply.github.com>
2015-11-15 22:09:59 -0600

    Mint setup pretty much working

--- mint/boostrap.sh
+++ mint/boostrap.sh
@@ -1,7 +1,47 @@
 # shut apt-get up
 DEBIAN_FRONTEND=noninteractive
 
-# later
+cd ~
+
+#INSTALLS
+
+: '
+# get programs from my README
+sudo apt-get -y install xbindkeys
+sudo apt-get -y install x11-xkb-utils # for setxkbmap for ditching CAPS
+sudo apt-get -y install stow
+# clang3.3 is already installed by anaconda. If I need clang3.6, I can use the full name
+sudo apt-get -y install zsh git
+sudo apt-get -y install build-essential python-dev cmake
+# I want to install clang-format and clang-modernize too
+'
+
+: '
+echo "get python from https://www.continuum.io/downloads"
+'
+
+# echo "get clang from ppa. it's on firefox somewhere"
+# echo "get firefox addons: vimperator, adblock plus, reddit enhancement suite"
+
+# eclipse
+: '
+# get eclipse luna
+sudo apt-get install openjdk-8-jdk
+# download eclipse to home folder
+cd ~
+#tar -xzvf name of tarfile
+# change how it looks in Window->Preferences->General->Appearance
+'
+
+# neovim
+: '
+sudo add-apt-repository ppa:neovim-ppa/unstable
+sudo apt-get update
+sudo apt-get install neovim
+# look at https://github.com/neovim/neovim/wiki/Installing-Neovim for python stuff
+'
+
+# i3wm
 : '
 # get i3wm. Maybe I should change this to the fork?
 sudo echo "deb http://debian.sur5r.net/i3/ $(lsb_release -c -s) universe" >> /etc/apt/sources.list
@@ -12,47 +52,18 @@ sudo apt-get install i3
 echo "@@@@@@@@@You can now log out and log back in to i3@@@@@@@@@"
 '
 
-# neovim
-: '
-sudo add-apt-repository ppa:neovim-ppa/unstable
-sudo apt-get update
-sudo apt-get install neovim
-# look at https://github.com/neovim/neovim/wiki/Installing-Neovim for python stuff
-'
-mkdir -p ${XDG_CONFIG_HOME:=$HOME/.config}
-ln -s ~/.vim $XDG_CONFIG_HOME/nvim
-ln -s ~/.vimrc $XDG_CONFIG_HOME/nvim/init.vim
-
-#later need to change to nvim
-: '
-sudo update-alternatives --install /usr/bin/editor editor /usr/bin/vim 1
-sudo update-alternatives --set editor /usr/bin/vim
-sudo update-alternatives --install /usr/bin/vi vi /usr/bin/vim 1
-sudo update-alternatives --set vi /usr/bin/vim
-echo "@@@@@@@@@@@@@ Vim is now built! @@@@@@@@@@@@@@"
-'
+# BUILDS
 
 # get oh-my-zsh
 : '
-sudo apt-get install zsh git
 wget --no-check-certificate http://install.ohmyz.sh -O - | sh
 chsh -s /bin/zsh
 echo "@@@@@@@@@@ ZSH installed. Logout and log back in!! @@@@@@@@@@@"
 '
-: '
-echo "get python from https://www.continuum.io/downloads"
-'
 
-: '
-# get programs from my README
-sudo apt-get -y install xbindkeys
-sudo apt-get -y install x11-xkb-utils # for setxkbmap for ditching CAPS
-sudo apt-get -y install stow
-# clang3.3 is already installed by anaconda. If I need clang3.6, I can use the full name
-sudo apt-get -y install clang-3.6
-# I want to install clang-format and clang-modernize too
-'
+# CONFIG
 
+# Stow common things
 : '
 # Now it takes sudo to change zsh stuff. Must update this. Later.
 # get config files
@@ -61,7 +72,24 @@ rm ~/.zshrc # ~/.i3/config
 stow common
 # move bootstrap that Ive downloaded and continually worked on to the repo
 stow mint
-# TODO: add change manually_symlink to ~/bin
+'
+
+# sudo ln -s /usr/bin/clang++-3.6 /usr/bin/clang++
+# sudo ln -s /usr/bin/vim /usr/bin/nvim
+
+# nvim things (I wan to try to make it compatible with vim)
+
+# These should be done after stowing the files :)
+# mkdir -p ${XDG_CONFIG_HOME:=$HOME/.config}
+# ln -s ~/.vim $XDG_CONFIG_HOME/nvim
+# ln -s ~/.vimrc $XDG_CONFIG_HOME/nvim/init.vim
+# ln -s ~/.vimrc-ben $XDG_CONFIG_HOME/nvim/vimrc-ben
+
+: '
+sudo update-alternatives --install /usr/bin/editor editor /usr/bin/nvim 1
+sudo update-alternatives --set editor /usr/bin/nvim
+sudo update-alternatives --install /usr/bin/vi vi /usr/bin/nvim 1
+sudo update-alternatives --set vi /usr/bin/nvim
 '
 
 : '
@@ -71,12 +99,14 @@ git config --global user.name "Ben-Mint"
 git config --global push.default matching
 '
 
-echo "make power commands non-sudo"
-echo "add this in /etc/sudoers via"
-echo "visudo"
-echo "%sudo ALL = NOPASSWD: /sbin/shutdown, /sbin/poweroff, /sbin/reboot"
-echo "dont forget the commas"
+# echo "make power commands non-sudo"
+# echo "add this in /etc/sudoers via"
+# echo "visudo"
+# echo "%sudo ALL = NOPASSWD: /sbin/shutdown, /sbin/poweroff, /sbin/reboot"
+# echo "dont forget the commas"
 
+
+# NON_IMPORTANT STUFF :D
 
 : '
 # haskell Stuff
@@ -98,22 +128,10 @@ echo "dont forget the commas"
 # sudo apt-get install numix-icon-theme numix-icon-theme-circle
 
 : '
-# get eclipse luna
-sudo apt-get install openjdk-8-jdk
-# download eclipse to home folder http://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/luna/SR1/eclipse-java-luna-SR1-linux-gtk-x86_64.tar.gz
-cd ~
-#tar -xzvf name of tarfile
-# change how it looks in Window->Preferences->General->Appearance
-'
-
-
-: '
 # start getting wallpapers!!
 cd ~
 git clone https://github.com/EndlesslyCurious/RedditImageGrab.git
 python2 ~/RedditImageGrab/redditdownload.py earthporn ~/Pictures/Wallpapers -score 1000 -num 25
 python2 ~/RedditImageGrab/redditdownload.py cityporn ~/Pictures/Wallpapers -score 1000 -num 25
 '
-
-#echo "get firefox addons: vimperator, adblock plus, Reddit Enhancement Suite"
 
