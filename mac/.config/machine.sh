675602959224ce2d38282f13bd571347f5320654
Benjamin Kane <bbk1524@gmail.com>
2016-10-03 13:11:46 -0500

    make mac work

--- mac/.config/machine.sh
+++ mac/.config/machine.sh
@@ -1,4 +1,9 @@
 
 # Should these go in .zshenv?
 export PATH="/Users/bbkane/anaconda3/bin:$PATH"
-[[ -e ~/perl5/perlbrew/etc/bashrc ]] && source ~/perl5/perlbrew/etc/bashrc
+
+perlbrew_bashrc="$HOME/perl5/perlbrew/etc/bashrc"
+[[ -e "${perlbrew_bashrc}" ]] && source "${perlbrew_bashrc}"
+
+stack_bin_dir="$HOME/.local/bin"
+[[ -d  "${stack_bin_dir}" ]] && export PATH="${stack_bin_dir}:$PATH"
