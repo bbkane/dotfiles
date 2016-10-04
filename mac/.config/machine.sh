2bd5f092331c37ca708e7bc757ab0286a5f5c9fd
Benjamin Kane <6081085+bbkane@users.noreply.github.com>
2016-10-03 22:27:05 -0500

    path stuff

--- mac/.config/machine.sh
+++ mac/.config/machine.sh
@@ -1,6 +1,10 @@
+# Homebrew
+homebrew_bin_dir="/usr/local/bin"
+export PATH="${homebrew_bin_dir}:$PATH"
 
 # Should these go in .zshenv?
-export PATH="/Users/bbkane/anaconda3/bin:$PATH"
+anaconda_bin_dir="/Users/bbkane/anaconda3/bin"
+export PATH="${anaconda_bin_dir}:$PATH"
 
 # make vim use ycm
 export vim_ide_status='ycm'
