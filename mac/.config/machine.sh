9345c81b8251746e5ae2399b9a39472718fc2fc1
Benjamin Kane <bbk1524@gmail.com>
2017-07-26 14:45:10 -0500

    Small tweaks

--- mac/.config/machine.sh
+++ mac/.config/machine.sh
@@ -7,6 +7,7 @@ stackoverflowit() {
 homebrew_bin_dir="/usr/local/bin"
 export PATH="${homebrew_bin_dir}:$PATH"
 
-export vim_ide_status='ycm'
+# YCM is being a *****
+# export vim_ide_status='ycm'
 
 complete -W 'start stop restart reload force-relead status configtest bootstrap' 'mysql.server'
