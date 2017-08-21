fa539f2fa8bea4dc7476e2ec7bca98690f71299b
Benjamin Kane <6081085+bbkane@users.noreply.github.com>
2017-08-21 18:11:12 -0500

    strengthen bash

--- templates/skeleton.sh
+++ templates/skeleton.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 # exit the script on command errors or unset variables
-set -eu
+# http://redsymbol.net/articles/unofficial-bash-strict-mode/
+set -euo pipefail
+IFS=$'\n\t'
 
 # https://stackoverflow.com/a/246128/295807
-# script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+# readonly script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
