8c3153f2889e9def31570b10aa8b271ded87eac2
Benjamin Kane <bbk1524@gmail.com>
2018-04-13 06:04:38 -0500

    update mac zsh stuff

--- mac/.config/machine.sh
+++ mac/.config/machine.sh
@@ -1,3 +1,5 @@
+#!/bin/zsh
+
 # pbcopy is mac specific
 stackoverflowit() {
     cat "$1"| sed 's/^/    /g' | pbcopy
@@ -11,3 +13,12 @@ export PATH="${homebrew_bin_dir}:$PATH"
 # export vim_ide_status='ycm'
 
 complete -W 'start stop restart reload force-relead status configtest bootstrap' 'mysql.server'
+
+export PATH="/Users/bbkane/anaconda3/bin:$PATH"
+
+[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
+
+export s5='192.168.1.66'
+export s8='192.168.1.153'
+
+export EDITOR=/usr/local/bin/nvim
