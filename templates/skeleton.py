ba192fa278a083a3ef9f180b9fb83c86dfa088ef
Benjamin Kane <bbk1524@gmail.com>
2018-07-09 11:13:04 -0700

    changes; about to clean up indent

--- templates/skeleton.py
+++ templates/skeleton.py
@@ -4,29 +4,6 @@
 __author__ = "Benjamin Kane"
 __version__ = "0.1.0"
 
-import argparse
-import sys
-
-__doc__ = """
-<description>
-
-Examples:
-
-    `{prog}`
-
-Help:
-
-Please see Benjamin Kane for help.
-Code at <repo>
-""".format(prog=sys.argv[0])
-
-
-def parse_args(*args, **kwargs):
-    parser = argparse.ArgumentParser(description=__doc__,
-                                     formatter_class=argparse.RawDescriptionHelpFormatter)
-    # TODO: add some args
-    return parser.parse_args(*args, **kwargs)
-
 
 def main():
     pass
