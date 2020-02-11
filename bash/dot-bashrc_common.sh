35c900d756ac8f77a3ee66aa7c6a2cf5fc1edb21
Benjamin Kane <6081085+bbkane@users.noreply.github.com>
2020-02-10 20:12:28 -0800

    add bash

---
+++ bash/dot-bashrc_common.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export PS1="\[\033[38;5;62m\]\$?\[$(tput sgr0)\]\[\033[38;5;15m\] \[$(tput sgr0)\]\[\033[38;5;27m\]\T\[$(tput sgr0)\]\[\033[38;5;15m\] \[$(tput sgr0)\]\[\033[38;5;33m\]\u\[$(tput sgr0)\]\[\033[38;5;15m\]@\[$(tput sgr0)\]\[\033[38;5;39m\]\h\[$(tput sgr0)\]\[\033[38;5;15m\]:\[$(tput sgr0)\]\[\033[38;5;45m\]\w\[$(tput sgr0)\]\[\033[38;5;15m\]\n\[$(tput sgr0)\]\[\033[38;5;123m\]λ\[$(tput sgr0)\]\[\033[38;5;15m\] \[$(tput sgr0)\]"
+
+# See ../zshrc/ for more stuff that might fit here :)
