de0c8d86346cef3d625e27476d6284307eb4f54e
Benjamin Kane <bbk1524@gmail.com>
2021-07-23 14:18:00 -0700

    organize the functions

--- bin_common/bin_common/chart.py
+++ bin_common/bin_common/chart.py
@@ -36,30 +36,7 @@ Repo: https://github.com/bbkane/dotfiles
     prog=pathlib.Path(sys.argv[0]).name
 )
 
-
-html = """
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-    <meta name="viewport" content="width=device-width,initial-scale=1">
-    <title>{output}</title>
-    <script src="https://cdn.plot.ly/plotly-latest.min.js"></script>
-  </head>
-  <body>
-    <div id="myplot"></div>
-    <script>
-    Plotly.plot(
-        "myplot",
-        JSON.parse('{plotly_data}'),
-        JSON.parse('{plotly_layout}'),
-        {{editable: false}}
-    );
-    </script>
-  </body>
-</html>
-"""
+# -- Plotly datastructures --
 
 
 class PlotlyTrace(ty.TypedDict):
@@ -80,22 +57,27 @@ class PlotlyLayout(ty.TypedDict, total=False):
     yaxis: ty.Optional[PlotlyAxis]
 
 
+# -- HTML formatting ---
+
+
 def html_div(div_id: str, plotly_data: ty.List[PlotlyTrace], plotly_layout: PlotlyLayout) -> str:
+    plotly_data_str = json.dumps(plotly_data)
+    plotly_layout_str = json.dumps(plotly_layout)
     div = """
     <div id="{div_id}"></div>
     <script>
     Plotly.plot(
         "{div_id}",
-        JSON.parse('{plotly_data}'),
-        JSON.parse('{plotly_layout}'),
+        JSON.parse('{plotly_data_str}'),
+        JSON.parse('{plotly_layout_str}'),
         {{editable: false}}
     );
     </script>
     """
     div = div.format(
         div_id=div_id,
-        plotly_data=plotly_data,
-        plotly_layout=plotly_layout,
+        plotly_data_str=plotly_data_str,
+        plotly_layout_str=plotly_layout_str,
     )
     return div
 
@@ -121,6 +103,79 @@ html_footer = """
   </body>
 </html>
 """
+
+
+# -- functions to build the plotly datastructures
+
+
+def csv_to_columns_with_fieldnames(inputfile: ty.TextIO, delimiter: str, fieldnames: ty.List[str]):
+    """Create columns when you already know the fieldnames"""
+    csvreader = csv.reader(inputfile, delimiter=delimiter)
+    first_line = next(csvreader)  # TODO: what if this is empty? Do I care?
+    assert len(first_line) == len(fieldnames)
+    return _csv_to_columns(inputfile, delimiter, fieldnames, first_line)
+
+
+def csv_to_columns_gen_fieldnames(inputfile: ty.TextIO, delimiter: str):
+    """Create columns and just auto-generate the fieldnames"""
+    csvreader = csv.reader(inputfile, delimiter=delimiter)
+    first_line = next(csvreader)  # TODO: what if this is empty? Do I care?
+    fieldnames = [f"field_{i}" for i in range(len(first_line))]
+    return _csv_to_columns(inputfile, delimiter, fieldnames, first_line)
+
+
+def csv_to_columns_first_line_as_fieldnames(inputfile: ty.TextIO, delimiter: str):
+    """Create columns and read fieldnames from first line"""
+    csvreader = csv.reader(inputfile, delimiter=delimiter)
+    fieldnames = next(csvreader)  # TODO: what if this is empty? Do I care?
+    first_line = next(csvreader)
+    return _csv_to_columns(inputfile, delimiter, fieldnames, first_line)
+
+
+def _csv_to_columns(
+    inputfile: ty.TextIO,
+    delimiter: str,
+    fieldnames: ty.List[str],
+    first_line: ty.List[str],
+) -> ty.DefaultDict[str, ty.List[str]]:
+    # add the first line
+    # NOTE: we can rely on insertion order, so iterating over keys later
+    # will work as expected
+    ret = collections.defaultdict(list)
+    for k, v in zip(fieldnames, first_line):
+        ret[k].append(v)
+
+    # add the rest of the lines
+    dictreader = csv.DictReader(inputfile, fieldnames=fieldnames, delimiter=delimiter)
+    for d_row in dictreader:
+        for k, v in d_row.items():
+            ret[k].append(v)
+
+    return ret
+
+
+def gen_timechart_json(columns: ty.Dict[str, ty.List]) -> ty.List[PlotlyTrace]:
+    # first column is datetime for xs (leave as string for now)
+    # TODO: the second version is optionally a grouping string
+    # rest of the columns should be numeric for ys
+
+    traces = []
+    column_names = tuple(columns.keys())
+    xs = columns[column_names[0]]
+    for name in column_names[1:]:
+        trace = PlotlyTrace(
+            mode="lines+markers",
+            name=name,
+            type="scatter",
+            x=xs,
+            y=columns[name],
+        )
+        traces.append(trace)
+
+    return traces
+
+
+# -- build the app and go --
 
 
 def parse_args(*args, **kwargs):
@@ -196,77 +251,12 @@ def parse_args(*args, **kwargs):
     return parser.parse_args(*args, **kwargs)
 
 
-def csv_to_columns_with_fieldnames(inputfile: ty.TextIO, delimiter: str, fieldnames: ty.List[str]):
-    """Create columns when you already know the fieldnames"""
-    csvreader = csv.reader(inputfile, delimiter=delimiter)
-    first_line = next(csvreader)  # TODO: what if this is empty? Do I care?
-    assert len(first_line) == len(fieldnames)
-    return _csv_to_columns(inputfile, delimiter, fieldnames, first_line)
-
-
-def csv_to_columns_gen_fieldnames(inputfile: ty.TextIO, delimiter: str):
-    """Create columns and just auto-generate the fieldnames"""
-    csvreader = csv.reader(inputfile, delimiter=delimiter)
-    first_line = next(csvreader)  # TODO: what if this is empty? Do I care?
-    fieldnames = [f"field_{i}" for i in range(len(first_line))]
-    return _csv_to_columns(inputfile, delimiter, fieldnames, first_line)
-
-
-def csv_to_columns_first_line_as_fieldnames(inputfile: ty.TextIO, delimiter: str):
-    """Create columns and read fieldnames from first line"""
-    csvreader = csv.reader(inputfile, delimiter=delimiter)
-    fieldnames = next(csvreader)  # TODO: what if this is empty? Do I care?
-    first_line = next(csvreader)
-    return _csv_to_columns(inputfile, delimiter, fieldnames, first_line)
-
-
-def _csv_to_columns(
-    inputfile: ty.TextIO,
-    delimiter: str,
-    fieldnames: ty.List[str],
-    first_line: ty.List[str],
-) -> ty.DefaultDict[str, ty.List[str]]:
-    # add the first line
-    # NOTE: we can rely on insertion order, so iterating over keys later
-    # will work as expected
-    ret = collections.defaultdict(list)
-    for k, v in zip(fieldnames, first_line):
-        ret[k].append(v)
-
-    # add the rest of the lines
-    dictreader = csv.DictReader(inputfile, fieldnames=fieldnames, delimiter=delimiter)
-    for d_row in dictreader:
-        for k, v in d_row.items():
-            ret[k].append(v)
-
-    return ret
-
-
-def gen_timechart_json(columns: ty.Dict[str, ty.List]) -> ty.List[PlotlyTrace]:
-    # first column is datetime for xs (leave as string for now)
-    # TODO: the second version is optionally a grouping string
-    # rest of the columns should be numeric for ys
-
-    traces = []
-    column_names = tuple(columns.keys())
-    xs = columns[column_names[0]]
-    for name in column_names[1:]:
-        trace = PlotlyTrace(
-            mode="lines+markers",
-            name=name,
-            type="scatter",
-            x=xs,
-            y=columns[name],
-        )
-        traces.append(trace)
-
-    return traces
-
-
 def main():
 
     # parse args
     args = parse_args()
+
+    # get core data
 
     with args.input_table:
         if args.fieldnames:
@@ -277,6 +267,10 @@ def main():
         else:  # default: generate some fieldnames...
             columns = csv_to_columns_gen_fieldnames(args.input_table, args.fieldsep)
 
+    plotly_data: ty.List[PlotlyTrace] = []
+    if args.subcommand_name == "timechart":
+        plotly_data = gen_timechart_json(columns)
+
     plotly_layout = PlotlyLayout()
     if args.title:
         plotly_layout["title"] = args.title
@@ -285,30 +279,28 @@ def main():
     if args.yaxis_title:
         plotly_layout["yaxis"] = PlotlyAxis(title=args.yaxis_title)
 
-    plotly_data = dict()
-    if args.subcommand_name == "timechart":
-        plotly_data = gen_timechart_json(columns)
-
-    formatted_html = html.format(
-        output=args.output,
-        plotly_data=json.dumps(plotly_data),
-        plotly_layout=json.dumps(plotly_layout),
-    )
-
     if not args.output:
         with tempfile.NamedTemporaryFile(
             mode="w",
             delete=False,
             suffix=".html",
         ) as fp:
-            print(formatted_html, file=fp)
+            print(html_header("TODO"), file=fp)
+            print(html_div("divID", plotly_data, plotly_layout), file=fp)
+            print(html_footer, file=fp)
             url = pathlib.Path(fp.name).as_uri()
         webbrowser.open_new_tab(url)
     elif args.output == "DATEME":
         right_now = datetime.datetime.now().strftime("%Y-%m-%d.%H.%M.%S")
         default_name = ".".join(["chart", right_now, "html"])
         with open(default_name, "w") as fp:
-            print(formatted_html, file=fp)
+            print(html_header("TODO"), file=fp)
+            print(html_div("divID", plotly_data, plotly_layout), file=fp)
+            print(html_footer, file=fp)
+    elif args.output.endswith(".div"):
+        div_id = args.output.removesuffix(".div")
+        div = html_div(div_id, plotly_data=plotly_data, plotly_layout=plotly_layout)
+        print(div)
     elif args.output.endswith(".json"):
         with open(args.output, "w") as fp:
             json.dump(
@@ -319,7 +311,9 @@ def main():
             )
     else:
         with open(args.output, "w") as fp:
-            print(formatted_html, file=fp)
+            print(html_header("TODO"), file=fp)
+            print(html_div("divID", plotly_data, plotly_layout), file=fp)
+            print(html_footer, file=fp)
 
 
 if __name__ == "__main__":
