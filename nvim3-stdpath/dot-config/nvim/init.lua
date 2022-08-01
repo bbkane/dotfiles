eee2ec84e6e321027658f60865a155df89890cd7
Benjamin Kane <6081085+bbkane@users.noreply.github.com>
2022-08-01 13:19:02 -0700

    migrate common settings

--- nvim3-stdpath/dot-config/nvim/init.lua
+++ nvim3-stdpath/dot-config/nvim/init.lua
@@ -1,5 +1,7 @@
 path = require("bbkane.path")
 
+-- Put all nvim files under ~/.config/nvim
+
 -- :h standard-paths
 vim.env.XDG_DATA_HOME = vim.fn.expand('~/.config/nvim/stdpath_data')
 
@@ -12,7 +14,10 @@ vim.env.XDG_DATA_HOME = vim.fn.expand('~/.config/nvim/stdpath_data')
 -- which was, vim.fn.stdpath("data") .. "/site", and now we need to just set
 -- that again now that we've modified the data dir
 
+-- this is needed for packer, but it's global so keep it here
 vim.opt.packpath:append(path.join(vim.fn.stdpath("data"), "site"))
+
+require("bbkane.common")
 
 -- install packer
 
