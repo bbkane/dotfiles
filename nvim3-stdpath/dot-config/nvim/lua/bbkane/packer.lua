509c5a91a6adac98adfbc611f8b220791f7a7dc7
Benjamin Kane <6081085+bbkane@users.noreply.github.com>
2022-08-03 16:38:33 -0700

    Add indentline

--- nvim3-stdpath/dot-config/nvim/lua/bbkane/packer.lua
+++ nvim3-stdpath/dot-config/nvim/lua/bbkane/packer.lua
@@ -73,6 +73,23 @@ return require('packer').startup(function(use)
         end,
     }
 
+    -- https://github.com/lukas-reineke/indent-blankline.nvim#with-context-indent-highlighted-by-treesitter
+    use {
+        "lukas-reineke/indent-blankline.nvim",
+        config = function()
+
+            vim.opt.list = true
+            vim.opt.listchars:append "space:⋅"
+            vim.opt.listchars:append "eol:↴"
+
+            require("indent_blankline").setup({
+                space_char_blankline = " ",
+                show_current_context = true,
+                show_current_context_start = true,
+            })
+        end,
+    }
+
     use {
         "lukas-reineke/lsp-format.nvim",
         config = function()
