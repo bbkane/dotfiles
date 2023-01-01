6247cfb72326ddec95c5c6fdd5e29049878d6f8a
Benjamin Kane <6081085+bbkane@users.noreply.github.com>
2023-01-01 11:13:33 -0800

    slightly better shell template

--- nvim3-stdpath/dot-config/nvim/templates/skeleton.sh
+++ nvim3-stdpath/dot-config/nvim/templates/skeleton.sh
@@ -6,5 +6,6 @@ set -euo pipefail
 IFS=$'\n\t'
 
 # https://stackoverflow.com/a/246128/295807
-# readonly script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+# script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+# readonly script_dir
 # cd "${script_dir}"
