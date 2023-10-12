43a7aade03c7f81334a43c02e1753c67d09f46e7
Benjamin Kane <6081085+bbkane@users.noreply.github.com>
2023-10-11 21:06:45 -0700

    tweaks, indent

--- wezterm/dot-config/wezterm/wezterm.lua
+++ wezterm/dot-config/wezterm/wezterm.lua
@@ -7,16 +7,17 @@ local config = {}
 -- In newer versions of wezterm, use the config_builder which will
 -- help provide clearer error messages
 if wezterm.config_builder then
-  config = wezterm.config_builder()
+    config = wezterm.config_builder()
 end
 
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
-config.color_scheme = 'kanagawabones'
+-- config.color_scheme = 'kanagawabones'
+config.color_scheme = '3024 (base16)'
 
-config.font = wezterm.font 'Monaco'
-config.font_size = 16.0
+config.font = wezterm.font('Monaco', { weight = 'Bold' })
+config.font_size = 17.0
 
 -- https://wezfurlong.org/wezterm/config/lua/window-events/format-tab-title.html
 -- https://wezfurlong.org/wezterm/config/lua/PaneInformation.html
@@ -25,22 +26,21 @@ config.font_size = 16.0
 -- Given "/foo/bar" returns "bar"
 -- Given "c:\\foo\\bar" returns "bar"
 function basename(s)
-  return string.gsub(s, '(.*[/\\])(.*)', '%2')
+    return string.gsub(s, '(.*[/\\])(.*)', '%2')
 end
 
 wezterm.on(
-  'format-tab-title',
-  function(tab, tabs, panes, config, hover, max_width)
-    local pane = tab.active_pane
-    -- NOTE: in the nightly, this will return a URL object, not a string
-    local cwd = pane.current_working_dir
-    local title = basename(cwd)
-    return {
-      { Text = ' ' .. title .. ' ' },
-    }
-  end
+    'format-tab-title',
+    function(tab, tabs, panes, config, hover, max_width)
+        local pane = tab.active_pane
+        -- NOTE: in the nightly, this will return a URL object, not a string
+        local cwd = pane.current_working_dir
+        local title = basename(cwd)
+        return {
+            { Text = ' ' .. title .. ' ' },
+        }
+    end
 )
 
 -- and finally, return the configuration to wezterm
 return config
-
