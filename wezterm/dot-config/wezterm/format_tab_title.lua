8d1d73b6094e72f356bdb1eda6855bffea27fe05
Benjamin Kane <92759008+bkane-msft@users.noreply.github.com>
2024-09-19 18:38:39 -0700

    use process_info

--- wezterm/dot-config/wezterm/format_tab_title.lua
+++ wezterm/dot-config/wezterm/format_tab_title.lua
@@ -79,11 +79,36 @@ local basename = function(s)
 	return string.gsub(s, "(.*[/\\])(.*)", "%2")
 end
 
+---@param process string
+---@param cwd string
+---@param is_active boolean
+---@return table
+local build_title = function (process, cwd, is_active)
+	local title = " " .. process .. " " .. cwd .. " "
+	-- TODO: use the whole args? https://wezfurlong.org/wezterm/config/lua/LocalProcessInfo.html
+	local color = hash_to_color(hash(process))
+
+	-- NOTE: my underline and intensity don't seem to be working...
+	local underline = "None"
+	local intensity = "Normal"
+	if is_active then
+		underline = "Double"
+		intensity = "Half"
+	end
+
+	-- https://wezfurlong.org/wezterm/config/lua/wezterm/format.html
+	return {
+		{ Attribute = { Intensity = intensity } },
+		{ Attribute = { Underline = underline } },
+		{ Background = { Color = color } },
+		{ Foreground = { Color = "black" } },
+		{ Text = title },
+	}
+end
+
+
 -- https://wezfurlong.org/wezterm/config/lua/window-events/format-tab-title.html
 function module.format_tab_title(tab, tabs, panes, config, hover, max_width)
-	-- https://wezfurlong.org/wezterm/config/lua/pane/index.html
-	local pane_info = tab.active_pane
-	local process = basename(pane_info.foreground_process_name)
 
 	-- I'll have to come back tot his when I have more time, but it's doing the righ thing!
 	-- -- https://wezfurlong.org/wezterm/config/lua/PaneInformation.html?h=foreground_
@@ -100,38 +125,28 @@ function module.format_tab_title(tab, tabs, panes, config, hover, max_width)
 	-- 	wezterm.log_info("process_info is nil")
 	-- end
 
+	local unknown = "<unknown>"
 
+	-- https://wezfurlong.org/wezterm/config/lua/pane/index.html
+	local pane_info = tab.active_pane
 	local cwd = pane_info.current_working_dir
+
 	-- cwd is nil if I pull up the Debug Overlay for example
 	if cwd == nil then
-		cwd = "<unknown>"
-		-- assume process is nil as well
-		process = "<unknown>"
-	else
-		-- cwd = string.gsub(cwd.file_path, wezterm.home_dir, '~')
-		cwd = basename(cwd.file_path)
+		-- assume process is also nil
+		return build_title(unknown, unknown, tab.is_active)
 	end
 
-	local title = " " .. process .. " " .. cwd .. " "
-	-- TODO: use the whole args? https://wezfurlong.org/wezterm/config/lua/LocalProcessInfo.html
-	local color = hash_to_color(hash(process))
+	cwd = basename(cwd.file_path)
 
-	-- NOTE: my underline and intensity don't seem to be working...
-	local underline = "None"
-	local intensity = "Normal"
-	if tab.is_active then
-		underline = "Double"
-		intensity = "Half"
-	end
+	-- local process = basename(pane_info.foreground_process_name)
+	local pane = mux.get_pane(pane_info.pane_id)
+	local process_info = pane:get_foreground_process_info()
+	local process_name = process_info.executable
+	process_name = basename(process_name)
 
-	-- https://wezfurlong.org/wezterm/config/lua/wezterm/format.html
-	return {
-		{ Attribute = { Intensity = intensity } },
-		{ Attribute = { Underline = underline } },
-		{ Background = { Color = color } },
-		{ Foreground = { Color = "black" } },
-		{ Text = title },
-	}
+	return build_title(process_name, cwd, tab.is_active)
 end
+
 
 return module
