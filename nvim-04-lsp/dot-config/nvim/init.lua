15d738d91c5395bf238a0af2883d7fc182c3d74d
Benjamin Kane <6081085+bbkane@users.noreply.github.com>
2025-12-26 21:36:44 -0800

    Lua LSP seems to work now

--- nvim-04-lsp/dot-config/nvim/init.lua
+++ nvim-04-lsp/dot-config/nvim/init.lua
@@ -31,7 +31,7 @@ require("lazy").setup({
   -- colorscheme that will be used when installing plugins.
   install = { colorscheme = { "habamax" } },
   -- automatically check for plugin updates
-  checker = { enabled = true },
+  -- checker = { enabled = true },
 })
 
 vim.lsp.enable({"lua_ls"})
